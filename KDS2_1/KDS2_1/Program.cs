55de182a8b802075bacf09ceee7566d2eebde9a5
andriuskin111 <andriuskin@gmail.com>
2019-12-20 21:03:21 +0200

    KDS2_1_v02

--- KDS2_1/KDS2_1/Program.cs
+++ KDS2_1/KDS2_1/Program.cs
@@ -10,6 +10,20 @@ namespace KDS2_1
     {
         static void Main(string[] args)
         {
+            double[] skaiciai = new double[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Iveskite {i + 1} skaiciu:");
+                skaiciai[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            for (int i = skaiciai.Length - 1; i >= 0; i--)
+            {
+                Console.Write(skaiciai[i]);
+            }
+          //  Console.WriteLine(skaiciai[1]);
+            Console.ReadLine();
         }
     }
 }
