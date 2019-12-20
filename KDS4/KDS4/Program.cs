acfe166949e458e3edafdef91d30b747c687ffe1
andriuskin111 <andriuskin@gmail.com>
2019-12-20 20:01:36 +0200

    KD3_v02

--- KDS4/KDS4/Program.cs
+++ KDS4/KDS4/Program.cs
@@ -10,6 +10,40 @@ namespace KDS4
     {
         static void Main(string[] args)
         {
+            double skaicius;
+            double rezultatas;
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("iveskite skaiciu:");
+
+                if(!double.TryParse(Console.ReadLine(), out skaicius))
+                {
+                    Console.WriteLine("Ivesti neteisingi duomenys");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine("Ivesto skaiciaus daugybos lentele:");
+
+                for (int i = 1; i < 11; i++)
+                {
+                    rezultatas = skaicius * i;
+                    Console.WriteLine(skaicius + " x " + i + " = " + rezultatas);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Testi betkoks mygtukas, Baigti spauskite [n]");
+                string klaviatura = Console.ReadLine();
+
+                if(klaviatura == "n")
+                {
+                    break;
+                }
+
+            }
+
         }
     }
 }
