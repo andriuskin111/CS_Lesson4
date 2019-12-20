cb9539f09378fa113236bb7d2cd50aa783127a00
andriuskin111 <andriuskin@gmail.com>
2019-12-20 19:23:21 +0200

    KDS3_v02

--- KDS3/KDS3/Program.cs
+++ KDS3/KDS3/Program.cs
@@ -10,6 +10,24 @@ namespace KDS3
     {
         static void Main(string[] args)
         {
+            int skaicius = 0;
+
+            Console.WriteLine("Iveskite skaiciu");
+
+            if (!int.TryParse(Console.ReadLine(), out skaicius))
+
+            {
+                Console.WriteLine("Ivesti neteisingi duomenys");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            for (int i = 0; i <= skaicius; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
         }
     }
 }
