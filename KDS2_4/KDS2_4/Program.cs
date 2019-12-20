377c610719b846805e8fb0902a4a557d573cc2fc
andriuskin111 <andriuskin@gmail.com>
2019-12-20 21:58:06 +0200

    KDS2_4_v02

--- KDS2_4/KDS2_4/Program.cs
+++ KDS2_4/KDS2_4/Program.cs
@@ -10,6 +10,18 @@ namespace KDS2_4
     {
         static void Main(string[] args)
         {
+            int[] skaiciai = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+                skaiciai[i] = rnd.Next(1, 100);
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                int maziausiai = skaiciai[i];
+            }
         }
     }
 }
