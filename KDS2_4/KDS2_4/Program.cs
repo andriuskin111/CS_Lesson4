2349c05448483135bd2d6c5e4efe4776d3595b5f
andriuskin111 <andriuskin@gmail.com>
2019-12-30 18:07:19 +0200

    KDS2_4_v03

--- KDS2_4/KDS2_4/Program.cs
+++ KDS2_4/KDS2_4/Program.cs
@@ -10,17 +10,80 @@ namespace KDS2_4
     {
         static void Main(string[] args)
         {
-            int[] skaiciai = new int[10];
+            int[] rndSkaiciai = new int[10];
+            int[] surusiuotiSkaiciai = new int[10];
+            int didziausiasSkaicius = 0;
+            int maziausiasSkaicius = 0;
+
             Random rnd = new Random();
 
-            for (int i = 0; i < skaiciai.Length; i++)
+            for (int i = 0; i < rndSkaiciai.Length; i++)
             {
-                skaiciai[i] = rnd.Next(1, 100);
+                rndSkaiciai[i] = rnd.Next(1, 100);
             }
 
-            for (int i = 0; i < 0; i++)
+            Console.Write("Atsitiktiniu skaiciu masyvas: ");
+
+            for (int i = 0; i < 10; i++)
             {
-                int maziausiai = skaiciai[i];
+                Console.Write($" {rndSkaiciai[i]} ");
+            }
+
+            rastiMaziausiaSkaiciu();
+            rastiDidziausiaSkaiciu();
+            surusiuotiMasyva();
+
+            Console.WriteLine();
+            Console.WriteLine($"Maziausias skaicius yra: {maziausiasSkaicius}");
+            Console.WriteLine($"Didziausias skaicius yra: {didziausiasSkaicius}");
+
+            Console.Write("Surusiuotu skaiciu masyvas: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($" {surusiuotiSkaiciai[i]} ");
+            }
+
+            Console.ReadLine();
+
+            int rastiMaziausiaSkaiciu()
+            {
+                maziausiasSkaicius = rndSkaiciai[0];
+
+                for (int i = 0; i < rndSkaiciai.Length - 1; i++)
+                {
+                    if (maziausiasSkaicius >= rndSkaiciai[i + 1])
+                    {
+                        maziausiasSkaicius = rndSkaiciai[i + 1];
+                    }
+                }
+
+                return maziausiasSkaicius;
+            }
+
+            int rastiDidziausiaSkaiciu()
+            {
+                didziausiasSkaicius = rndSkaiciai[0];
+
+                for (int i = 0; i < rndSkaiciai.Length - 1; i++)
+                {
+                    if (didziausiasSkaicius <= rndSkaiciai[i + 1])
+                    {
+                        didziausiasSkaicius = rndSkaiciai[i + 1];
+                    }
+                }
+
+                return didziausiasSkaicius;
+            }
+
+            void surusiuotiMasyva()
+            {
+                Array.Sort(rndSkaiciai);
+
+                for (int i = 0; i < rndSkaiciai.Length; i++)
+                {
+                    surusiuotiSkaiciai[i] = rndSkaiciai[i];
+                }
             }
         }
     }
