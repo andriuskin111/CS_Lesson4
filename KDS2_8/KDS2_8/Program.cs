231d95dc32405f7d9d824ee1809d5d27db75c1c4
andriuskin111 <andriuskin@gmail.com>
2019-12-31 01:24:08 +0200

    KDS2_8_v02

--- KDS2_8/KDS2_8/Program.cs
+++ KDS2_8/KDS2_8/Program.cs
@@ -10,6 +10,27 @@ namespace KDS2_8
     {
         static void Main(string[] args)
         {
+            int[] skaiciuMasyvas = new int[] { 1, 9, 6, 7, 5, 9 , 12, 15, 90, 2, -1};
+
+            surusiuotiMasyva();
+
+            Console.WriteLine($"Antras didziausiai skaicius: {skaiciuMasyvas[skaiciuMasyvas.Length - 2]}");
+            Console.WriteLine($"Antras maziausias skaicius: {skaiciuMasyvas[1]}");
+            Console.ReadLine();
+
+            void surusiuotiMasyva()
+            {
+                for (int i = 0; i < skaiciuMasyvas.Length - 1; i++)
+                {
+                    for (int k = i + 1; k < skaiciuMasyvas.Length; k++)
+                    {
+                        if (skaiciuMasyvas[i] > skaiciuMasyvas[k])
+                        {
+                            (skaiciuMasyvas[i], skaiciuMasyvas[k]) = (skaiciuMasyvas[k], skaiciuMasyvas[i]);
+                        }
+                    }
+                }
+            }       
         }
     }
 }
