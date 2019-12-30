6ec52d54d3b7bf8d6d98e417a95d7ca47e449ca7
andriuskin111 <andriuskin@gmail.com>
2019-12-31 00:26:41 +0200

    KDS2_7_v02

--- KDS2_7/KDS2_7/Program.cs
+++ KDS2_7/KDS2_7/Program.cs
@@ -10,6 +10,47 @@ namespace KDS2_7
     {
         static void Main(string[] args)
         {
+            int[] pirmasMasyvas = new int[10];
+            int indeksas;
+            
+            Random rnd = new Random();
+
+            for (int i = 0; i < pirmasMasyvas.Length; i++)
+            {
+                pirmasMasyvas[i] = rnd.Next(1, 100);
+            }
+
+            Console.Write("Atsitiktiniu skaiciu masyvas: ");
+
+            for (int i = 0; i < pirmasMasyvas.Length; i++)
+            {
+                Console.Write($" {pirmasMasyvas[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pasirinkite masyvo elementa kuri norite istrinti: ");
+
+            indeksas = int.Parse(Console.ReadLine());
+
+            for (int i = indeksas; i < pirmasMasyvas.Length - 1; i++)
+            {
+                pirmasMasyvas[i] = pirmasMasyvas[i + 1];
+            }
+
+            Array.Resize(ref pirmasMasyvas, pirmasMasyvas.Length - 1);
+
+            int[] antrasMasyvas = new int[pirmasMasyvas.Length];
+
+            Array.Copy(pirmasMasyvas, 0, antrasMasyvas, 0, pirmasMasyvas.Length);
+
+            Console.Write("Masyvas be istrinto elemento: ");
+
+            for (int i = 0; i < antrasMasyvas.Length; i++)
+            {
+                Console.Write($" {antrasMasyvas[i]} ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
