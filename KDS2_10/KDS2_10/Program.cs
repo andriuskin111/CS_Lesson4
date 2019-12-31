6996cfe85067ef089202a2e79581239df0e479f8
andriuskin111 <andriuskin@gmail.com>
2019-12-31 02:14:21 +0200

    KDS2_10_v01

---
+++ KDS2_10/KDS2_10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDS2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciuMasyvas = { 2, 7, 19, 50, -4, -7, 11, -4, 2, 7 };
+
+            Console.Write("Nesurusiuotas masyvas: ");
+
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                Console.Write($" {skaiciuMasyvas[i]} ");
+            }
+
+            Console.WriteLine();
+
+            surusiuotiMasyva();
+
+            Console.Write("Surusiuotas masyvas: ");
+
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                Console.Write($" {skaiciuMasyvas[i]} ");
+            }
+
+            Console.ReadLine();
+
+            void surusiuotiMasyva()
+            {
+                for (int i = 0; i < skaiciuMasyvas.Length - 1; i++)
+                {
+                    for (int k = i + 1; k < skaiciuMasyvas.Length; k++)
+                    {
+                        if (skaiciuMasyvas[i] > skaiciuMasyvas[k])
+                        {
+                            (skaiciuMasyvas[i], skaiciuMasyvas[k]) = (skaiciuMasyvas[k], skaiciuMasyvas[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
