b35f04d52e1b6edae27677481453ba47c24a0236
andriuskin111 <andriuskin@gmail.com>
2020-01-01 20:04:21 +0200

    ND_Masyvai_10_v02

--- ND_Masyvai_10/ND_Masyvai_10/Program.cs
+++ ND_Masyvai_10/ND_Masyvai_10/Program.cs
@@ -10,6 +10,37 @@ namespace ND_Masyvai_10
     {
         static void Main(string[] args)
         {
+            string[] vardai = {
+                "Andrius",
+                "Giedrius",
+                "Ieva",
+                "Aleksandras",
+                "Vilija",
+                "Romanas",
+                "Gintare",
+                "Egle",
+                "Vasia",
+                "Paulina"
+            };
+            string[] isimtiVardai = new string[8];
+
+            Array.Copy(vardai, 0, isimtiVardai, 0, 5);
+            Array.Copy(vardai, 6, isimtiVardai, 5, 1);
+            Array.Copy(vardai, 8, isimtiVardai, 6, 2);
+
+            for (int i = 0; i < vardai.Length; i++)
+            {
+                Console.WriteLine($"{i} {vardai[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < isimtiVardai.Length; i++)
+            {
+                Console.WriteLine($"{i} {isimtiVardai[i]}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
