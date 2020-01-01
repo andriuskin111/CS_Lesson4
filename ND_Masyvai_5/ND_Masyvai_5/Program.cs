6cafeb5f63b44c22aad249d5a5b51d9835dbca1b
andriuskin111 <andriuskin@gmail.com>
2020-01-01 18:12:14 +0200

    ND_Masyvai_5_v02

--- ND_Masyvai_5/ND_Masyvai_5/Program.cs
+++ ND_Masyvai_5/ND_Masyvai_5/Program.cs
@@ -10,6 +10,20 @@ namespace ND_Masyvai_5
     {
         static void Main(string[] args)
         {
+            string vardas = "Andrius";
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                Console.Write($"{vardas[i]} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = vardas.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{vardas[i]} ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
