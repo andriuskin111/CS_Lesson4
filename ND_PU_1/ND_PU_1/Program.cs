37608ac71ddddf89dc73ee22cb2736e8986689fb
andriuskin111 <andriuskin@gmail.com>
2020-01-01 20:23:45 +0200

    ND_PU_1_v01

---
+++ ND_PU_1/ND_PU_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND_PU_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] vardai = new string[6, 2]
+            {
+                {"Beyonce", "m" },
+                {"David Bowie", "v" },
+                {"Elvis Costello", "v" },
+                {"Madonna", "m" },
+                {"Elton John", "v" },
+                {"Charles Aznavor", "v" }
+            };
+
+            Console.WriteLine("Vyrai:");
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (vardai[i, 1] == "v")
+                {
+                    Console.WriteLine(vardai[i, 0]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Moterys:");
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (vardai[i, 1] == "m")
+                {
+                    Console.WriteLine(vardai[i, 0]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
