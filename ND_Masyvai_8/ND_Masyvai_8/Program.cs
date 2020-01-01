7916e85ad0267156e20224af0463cdb76e5162d7
andriuskin111 <andriuskin@gmail.com>
2020-01-01 19:02:51 +0200

    ND_Masyvai_8_v02

--- ND_Masyvai_8/ND_Masyvai_8/Program.cs
+++ ND_Masyvai_8/ND_Masyvai_8/Program.cs
@@ -10,6 +10,42 @@ namespace ND_Masyvai_8
     {
         static void Main(string[] args)
         {
+            int[] skaiciuMasyvas = new int[5];
+
+            //irasome vartotojo ivestus duomenis i pirma masyva
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                Console.WriteLine($"Iveskite skaiciu nr {i + 1}");
+
+                if (!int.TryParse(Console.ReadLine(), out skaiciuMasyvas[i]))
+                {
+                    Console.WriteLine("Ivesti neteisingi duomenys \n");
+                    i--;
+                }
+            }
+
+            //isvedam i konsole nesurusiuota masyva
+            Console.Write("Jusu ivestas masyvas: ");
+
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                Console.Write($" {skaiciuMasyvas[i]}");
+            }
+
+            Console.WriteLine();
+
+            //surusiuojam ir isvedam masyva i konsole
+            Array.Sort(skaiciuMasyvas);
+
+            Console.Write($"Surusiuotas masyvas: ");
+
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                Console.Write($" {skaiciuMasyvas[i]}");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
