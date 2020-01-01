6b7e22b94e2a34e2a58a42b3d0074f32c310fff1
andriuskin111 <andriuskin@gmail.com>
2020-01-01 17:51:40 +0200

    ND_Masyvai_1_v02

--- ND_Masyvai_1/ND_Masyvai_1/Program.cs
+++ ND_Masyvai_1/ND_Masyvai_1/Program.cs
@@ -10,6 +10,14 @@ namespace ND_Masyvai_1
     {
         static void Main(string[] args)
         {
+            int[] masyvas = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(masyvas[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
