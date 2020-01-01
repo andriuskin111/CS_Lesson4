7b214ad58a1c2d12d49dfca47d1199203b5af1f2
andriuskin111 <andriuskin@gmail.com>
2020-01-01 18:07:09 +0200

    ND_Masyvai_4_v02

--- ND_Masyvai_4/ND_Masyvai_4/Program.cs
+++ ND_Masyvai_4/ND_Masyvai_4/Program.cs
@@ -10,6 +10,19 @@ namespace ND_Masyvai_4
     {
         static void Main(string[] args)
         {
+            string[] masyvas = { "a", "b", "c", "d", "e", "f" };
+
+            //for (int i = 0; i < masyvas.Length; i++)
+            //{
+            //    Console.Write($"{masyvas[i]} ");
+            //}
+
+            for (int i = masyvas.Length - 1; i <= 0; i--)
+            {
+                Console.Write($"{masyvas[i]} ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
