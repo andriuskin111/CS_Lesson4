6796e3b615be7007a61c7d2980985604b06a941f
andriuskin111 <andriuskin@gmail.com>
2020-01-01 18:26:19 +0200

    ND_Masyvai_6_v02

--- ND_Masyvai_6/ND_Masyvai_6/Program.cs
+++ ND_Masyvai_6/ND_Masyvai_6/Program.cs
@@ -10,6 +10,56 @@ namespace ND_Masyvai_6
     {
         static void Main(string[] args)
         {
+            int[] skaiciuMasyvas = new int[100];
+            int maziausiasSkaicius = 0;
+            int didziausiasSkaicius = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < skaiciuMasyvas.Length; i++)
+            {
+                skaiciuMasyvas[i] = rnd.Next(1, 1000);
+            }
+
+            rastiMaziausiaSkaiciu();
+            rastiDidziausiaSkaiciu();
+
+            Console.WriteLine($"Maziausias skaicius yra: {maziausiasSkaicius}");
+            Console.WriteLine($"Didziausias skaicius yra: {didziausiasSkaicius}");
+
+            Console.ReadLine();
+
+            int rastiMaziausiaSkaiciu()
+            {
+                maziausiasSkaicius = skaiciuMasyvas[0];
+
+                for (int i = 0; i < skaiciuMasyvas.Length - 1; i++)
+                {
+                    if (maziausiasSkaicius >= skaiciuMasyvas[i + 1])
+                    {
+                        maziausiasSkaicius = skaiciuMasyvas[i + 1];
+                    }
+                }
+
+                return maziausiasSkaicius;
+            }
+
+
+
+            int rastiDidziausiaSkaiciu()
+            {
+                didziausiasSkaicius = skaiciuMasyvas[0];
+
+                for (int i = 0; i < skaiciuMasyvas.Length - 1; i++)
+                {
+                    if (didziausiasSkaicius <= skaiciuMasyvas[i + 1])
+                    {
+                        didziausiasSkaicius = skaiciuMasyvas[i + 1];
+                    }
+                }
+
+                return didziausiasSkaicius;
+            }
         }
     }
 }
