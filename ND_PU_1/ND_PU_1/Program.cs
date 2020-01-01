7990431233f11ac9a8a4e7792bce6ac21f2bcf85
andriuskin111 <andriuskin@gmail.com>
2020-01-01 21:29:44 +0200

    ND_PU_1_v02

--- ND_PU_1/ND_PU_1/Program.cs
+++ ND_PU_1/ND_PU_1/Program.cs
@@ -10,34 +10,67 @@ namespace ND_PU_1
     {
         static void Main(string[] args)
         {
-            string[,] vardai = new string[6, 2]
+            //----------------------------------------pirmas budas (blogas)-----------------------------------------
+            //string[,] vardai = new string[6, 2]
+            //{
+            //    {"Beyonce", "m" },
+            //    {"David Bowie", "v" },
+            //    {"Elvis Costello", "v" },
+            //    {"Madonna", "m" },
+            //    {"Elton John", "v" },
+            //    {"Charles Aznavor", "v" }
+            //};
+
+            //Console.WriteLine("Vyrai:");
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    if (vardai[i, 1] == "v")
+            //    {
+            //        Console.WriteLine(vardai[i, 0]);
+            //    }
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("Moterys:");
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    if (vardai[i, 1] == "m")
+            //    {
+            //        Console.WriteLine(vardai[i, 0]);
+            //    }
+            //}
+
+            //-----------------------------------antras budas (geresnis)------------------------------------------
+            string[] vardai = 
             {
-                {"Beyonce", "m" },
-                {"David Bowie", "v" },
-                {"Elvis Costello", "v" },
-                {"Madonna", "m" },
-                {"Elton John", "v" },
-                {"Charles Aznavor", "v" }
-            };
+                "Beyonce (m)",
+                "David Bowie (v)",
+                "Elvis Costello (v)",
+                "Madonna (m)",
+                "Elton John (v)",
+                "Charles Aznavor (v)" 
+                };
 
             Console.WriteLine("Vyrai:");
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < vardai.Length; i++)
             {
-                if (vardai[i, 1] == "v")
+                if (vardai[i].Contains("(v)"))
                 {
-                    Console.WriteLine(vardai[i, 0]);
+                    Console.WriteLine(vardai[i].Remove(vardai[i].Length - 3));
                 }
             }
 
             Console.WriteLine();
             Console.WriteLine("Moterys:");
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < vardai.Length; i++)
             {
-                if (vardai[i, 1] == "m")
+                if (vardai[i].Contains("(m)"))
                 {
-                    Console.WriteLine(vardai[i, 0]);
+                    Console.WriteLine(vardai[i].Remove(vardai[i].Length -3));
                 }
             }
 
