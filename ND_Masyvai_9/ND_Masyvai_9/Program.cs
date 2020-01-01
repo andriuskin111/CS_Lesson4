cecd37a0dcaf09dfbd1cc1b3d024440920e5ecbe
andriuskin111 <andriuskin@gmail.com>
2020-01-01 19:20:48 +0200

    ND_Masyvai_9_v02

--- ND_Masyvai_9/ND_Masyvai_9/Program.cs
+++ ND_Masyvai_9/ND_Masyvai_9/Program.cs
@@ -10,6 +10,49 @@ namespace ND_Masyvai_9
     {
         static void Main(string[] args)
         {
+            int[] pirmasMasyvas = { 1, 2, 3 };
+            int[] antrasMasyvas = { 4, 5, 6 };
+            int[] treciasMasyvas = new int[pirmasMasyvas.Length + antrasMasyvas.Length];
+
+            //parodom pirma ir antra masyvus
+            Console.Write("Pirmas masyvas: ");
+
+            for (int i = 0; i < pirmasMasyvas.Length; i++)
+            {
+                Console.Write($" {pirmasMasyvas[i]}");
+            }
+
+            Console.WriteLine();
+            Console.Write("Pirmas masyvas: ");
+
+            for (int i = 0; i < antrasMasyvas.Length; i++)
+            {
+                Console.Write($" {antrasMasyvas[i]}");
+            }
+
+            Console.WriteLine();
+
+            //kopijuojam pirma masyva i trecia
+            for (int i = 0; i < pirmasMasyvas.Length; i++)
+            {
+                treciasMasyvas[i] = pirmasMasyvas[i];
+            }
+
+            //kopijuojam antra masyva i trecia
+            for (int i = 0; i < antrasMasyvas.Length; i++)
+            {
+                treciasMasyvas[i+ pirmasMasyvas.Length] = antrasMasyvas[i];
+            }
+
+            //parodom trecia masyva
+            Console.Write("Trecias masyvas: ");
+
+            for (int i = 0; i < treciasMasyvas.Length; i++)
+            {
+                Console.Write($" {treciasMasyvas[i]}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
