07a8c353f571fa2c91f54bd4d91b6de78250cf72
andriuskin111 <andriuskin@gmail.com>
2020-01-03 22:36:19 +0200

    KD_List_1_v02

--- KD_List_1/KD_List_1/Program.cs
+++ KD_List_1/KD_List_1/Program.cs
@@ -10,6 +10,24 @@ namespace KD_List_1
     {
         static void Main(string[] args)
         {
-        }
+            List<int> skaiciuSarasas = new List<int>();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                skaiciuSarasas.Add(rnd.Next(1, 100));
+            }
+
+            foreach (var item in skaiciuSarasas)
+            {
+                if(item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }               
+            }
+
+            Console.ReadLine();
+        }        
     }
 }
