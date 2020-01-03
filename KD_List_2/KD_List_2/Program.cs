cd8ff4df136cf4abfa9b4c57e3bac6fa9e5e0d77
andriuskin111 <andriuskin@gmail.com>
2020-01-03 23:00:15 +0200

    KD_List_2_v02

--- KD_List_2/KD_List_2/Program.cs
+++ KD_List_2/KD_List_2/Program.cs
@@ -10,6 +10,40 @@ namespace KD_List_2
     {
         static void Main(string[] args)
         {
+            List<string> studentuSarasas = new List<string>();
+            studentuSarasas.Add("Antanas Juknevicius");
+            studentuSarasas.Add("Vaidotas Zala");
+            studentuSarasas.Add("Benediktas Vanagas");
+            studentuSarasas.Add("Edvardas Lucevicius");
+            studentuSarasas.Add("Vytautas Svedas");
+
+            List<string> studentuSarasasPavelavusiu = new List<string>();
+            studentuSarasasPavelavusiu.Add("Andrius Petrauskas");
+            studentuSarasasPavelavusiu.Add("Vladas Ciunelis");
+            studentuSarasasPavelavusiu.Add("Eldar Ushadov");
+
+            Console.WriteLine("Studentu sarasas:");
+            parodytiSarasa();
+            Console.WriteLine();
+
+            studentuSarasas.AddRange(studentuSarasasPavelavusiu);
+            Console.WriteLine("Papildytas studentu sarasas:");
+            parodytiSarasa();
+            Console.WriteLine();
+
+            studentuSarasas.Sort();
+            Console.WriteLine("Surusiuotas studentu sarasas:");
+            parodytiSarasa();
+
+            void parodytiSarasa()
+            {
+                foreach (var item in studentuSarasas)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
