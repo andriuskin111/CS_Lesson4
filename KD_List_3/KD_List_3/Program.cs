c2533d310f26e08969203b261185284ae61beffe
andriuskin111 <andriuskin@gmail.com>
2020-01-03 23:20:06 +0200

    KD_List_3_v02

--- KD_List_3/KD_List_3/Program.cs
+++ KD_List_3/KD_List_3/Program.cs
@@ -10,6 +10,37 @@ namespace KD_List_3
     {
         static void Main(string[] args)
         {
+            List<string> varduSarasas = new List<string>();
+            varduSarasas.Add("Natalija");
+            varduSarasas.Add("Vytautas");
+            varduSarasas.Add("Gabija");
+            varduSarasas.Add("Vasia");
+            varduSarasas.Add("Ieva");
+
+            Console.WriteLine("Pirminis sarasas:");
+            parodytiSarasa();
+            Console.WriteLine();
+
+            varduSarasas.Add("Napoleonas");
+            varduSarasas.Add("Asterisas");
+            Console.WriteLine("Papildytas sarasas:");
+            parodytiSarasa();
+            Console.WriteLine();
+
+            varduSarasas.RemoveAt(varduSarasas.IndexOf("Napoleonas"));
+            varduSarasas.Insert(varduSarasas.IndexOf("Ieva") + 1, "Kleopatra");
+            Console.WriteLine("Redaguotas sarasas:");
+            parodytiSarasa();
+
+            void parodytiSarasa()
+            {
+                foreach (var item in varduSarasas)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
