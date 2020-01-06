038fef9d3d94ac358e77c0a7fb34f78331175c83
andriuskin111 <andriuskin@gmail.com>
2020-01-06 21:41:26 +0200

    pridetas saraso rusiavimo taimeris

--- KD_List_7/KD_List_7/Program.cs
+++ KD_List_7/KD_List_7/Program.cs
@@ -10,6 +10,22 @@ namespace KD_List_7
     {
         static void Main(string[] args)
         {
+            List<int> sarasas = new List<int>();
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < 100000; i++)
+            {
+                sarasas.Add(rnd.Next(1, 1000));
+            }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            sarasas.Sort();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine(elapsedMs);
+            Console.ReadLine();
         }
     }
 }
