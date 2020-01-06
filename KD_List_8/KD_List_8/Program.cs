3adb9a6cd2841c6c2ace68f8d0e0fe2df99c00e5
andriuskin111 <andriuskin@gmail.com>
2020-01-06 22:48:57 +0200

    Skaitymas is txt rusiavimas ir irasymas i kita txt

--- KD_List_8/KD_List_8/Program.cs
+++ KD_List_8/KD_List_8/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace KD_List_8
 {
@@ -10,6 +11,37 @@ namespace KD_List_8
     {
         static void Main(string[] args)
         {
+            string textLine;
+
+            try
+            {
+                StreamReader reader = new StreamReader("C:/Users/1011/Documents/Visual Studio 2019/Dot NET Kursai/Countries.txt");
+                textLine = reader.ReadLine();
+                Console.WriteLine(textLine);
+
+                string[] separator = { ", " };
+                string[] textColum = textLine.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+                Array.Sort(textColum);
+                using (StreamWriter writer = new StreamWriter("C:/Users/1011/Documents/Visual Studio 2019/Dot NET Kursai/Countries_sorted.txt"))
+                {
+                    foreach (var item in textColum)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+                    
+
+                
+                
+            }
+
+            catch
+            {
+                Console.WriteLine("File not found");
+            }
+
+            Console.ReadLine();
         }
     }
 }
